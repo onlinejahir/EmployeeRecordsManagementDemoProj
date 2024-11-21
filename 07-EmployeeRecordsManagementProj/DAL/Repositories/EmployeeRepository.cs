ad9f5b0574cecfd2bcd03c6cd82c2f7f5ef7a010
onlinejahir <document.jahir@gmail.com>
2024-11-21 16:09:59 +0800

    added eagerloading

--- 07-EmployeeRecordsManagementProj/DAL/Repositories/EmployeeRepository.cs
+++ 07-EmployeeRecordsManagementProj/DAL/Repositories/EmployeeRepository.cs
@@ -1,6 +1,7 @@
 ﻿using _07_EmployeeRecordsManagementProj.DAL.Contracts;
 using _07_EmployeeRecordsManagementProj.Data;
 using _07_EmployeeRecordsManagementProj.EntityModels;
+using Microsoft.EntityFrameworkCore;
 
 namespace _07_EmployeeRecordsManagementProj.DAL.Repositories
 {
@@ -10,6 +11,12 @@ namespace _07_EmployeeRecordsManagementProj.DAL.Repositories
         public EmployeeRepository(ApplicationDbContext dbContext) : base(dbContext)
         {
             this._dbContext = dbContext;
+        }
+
+        public override async Task<IQueryable<Employee>> GetAllAsync()
+        {
+            return await Task.FromResult(_dbContext.Employees
+                .Include(e => e.Department).AsQueryable());
         }
     }
 }
